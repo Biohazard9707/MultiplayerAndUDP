b1f649f4a8d7641d6660500cbf3d7e432860d4d5
Biohazard <hugo.enscruz@gmail.com>
2019-07-25 15:44:22 -0500

    Se modificó el script PlayerController para asignarle un color al jugador local version 1.9

--- Assets/My Scripts/PlayerController.cs
+++ Assets/My Scripts/PlayerController.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//Importamos la libreria Networking para poder
-//trabajar con los metodos del Administrador de Red(Networking Manager)
+
+/*Importamos la libreria Networking para poder trabajar 
+con los metodos del Administrador de Red(Networking Manager)*/
 using UnityEngine.Networking;
 
-//Aplicamos herencia sobre la clase PlayerController
-//po lo cual reemplazamos "MonoBehaviour" por "NetworkBehaviour"
-//Esto nos permitira hacer uso de la clase network
+/*Aplicamos herencia sobre la clase PlayerController
+po lo cual reemplazamos "MonoBehaviour" por "NetworkBehaviour"
+Esto nos permite hacer uso de la clase Network*/
+
 public class PlayerController : NetworkBehaviour
 {
 
@@ -20,26 +22,41 @@ public class PlayerController : NetworkBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        //Simple condición que verifica si eres el jugador local
-        //de no serlo sale inmediatamente del metodo update
-        //Por lo cual las funciones solo se aplicaran para el oojeto(jugador)
-        //que controlas.
+        /*Simple condición que verifica si eres el jugador local
+        de no serlo sale inmediatamente del metodo update. Por lo 
+        cual las funciones solo se aplicaran para el objeto(jugador)
+        que controlas.*/
         if(!isLocalPlayer)
         {
             return;
         }
 
-        //Variable que le otorgara al jugador movilidad de rotación en el eje x con respecto
-        // a las teclas de direccion derecha e izquierda
+        /*Variable que le otorgara al jugador movilidad de rotación en el 
+        eje x con respecto a las teclas de direccion derecha e izquierda.*/
         float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        //Variable que le otorgara al jugador movilidad de translación en el eje z con respecto
-        // a las teclas de direccion arriba y abajo
+
+        /*Variable que le otorgara al jugador movilidad de translación en 
+        el eje z con respecto a las teclas de direccion arriba y abajo.*/
         float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
-        //metodo que le pasa como parametro "x" en el eje x para rotar el objeto
+        //método que le pasa como parametro "x" en el eje x para rotar el objeto
         transform.Rotate(0, x, 0);
-        //metodo que se le pasa como parametro "Z" en el eje z para trasladar el objeto 
+        //método que se le pasa como parametro "Z" en el eje z para trasladar el objeto 
         transform.Translate(0, 0, z);
 
 	}
+
+    /*Utilizamos el metodo OnStartLocalPlayer() y utlizamos override para extender o modificar
+    la implementación abstracta o virtual de un método, propiedad, indizador o evento heredado.*/
+    public override void OnStartLocalPlayer()
+    {
+        //base.OnStartLocalPlayer();
+
+        /*Obtenemos el componente Material de 
+         PlayerPrefab que estamos controlando 
+         como cliente y le asignamos un nuevo
+         color cuando lo instanciamos por
+         primera vez.*/
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
 }
