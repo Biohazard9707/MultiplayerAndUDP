60e6445011c4561a2939a64ec24e7925cb8c8452
Biohazard <hugo.enscruz@gmail.com>
2019-07-29 17:47:35 -0500

    Se agrego el movimiento del jugador y se aplicaron los cambios necesarios para el funcionamiento de NetworkManager

---
+++ Assets/ScriptsGoKart/PlayerControllerr.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerControllerr : NetworkBehaviour {
+
+    private float x, z;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
