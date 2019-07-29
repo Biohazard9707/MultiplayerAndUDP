01deed9de33f04e2bd52f028566a3b6c63a1ec47
Biohazard <hugo.enscruz@gmail.com>
2019-07-29 18:17:34 -0500

    Se agrego independencia entre ambos jugadores y se confiro el script PlayerCntrollerr

---
+++ Assets/My Scripts/Pruebas.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pruebas : MonoBehaviour {
+
+    public Texture changeTexture;
+	// Use this for initialization
+	void Start () {
+        GameObject body = transform.GetChild(4).gameObject;
+        Debug.Log(body);
+        body.GetComponent<MeshRenderer>().material.mainTexture = changeTexture;
+        //GetComponent<MeshRenderer>().material.mainTexture = changeTexture;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/ScriptsGoKart/PlayerControllerr.cs
+++ Assets/ScriptsGoKart/PlayerControllerr.cs
@@ -6,6 +6,7 @@ using UnityEngine.Networking;
 public class PlayerControllerr : NetworkBehaviour {
 
     private float x, z;
+    public Texture changeTexture;
 
     // Use this for initialization
     void Start () {
@@ -14,10 +15,23 @@ public class PlayerControllerr : NetworkBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        if(!isLocalPlayer)
+        {
+            return;
+        }
+
         x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
     }
+
+    public override void OnStartLocalPlayer()
+    {
+        GameObject body = transform.GetChild(4).gameObject;
+        body.GetComponent<MeshRenderer>().material.mainTexture = changeTexture;
+    }
+
 }
