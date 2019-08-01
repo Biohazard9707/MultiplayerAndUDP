91833390a03ec8aeae3f6e861a17ff4ae49117a8
Biohazard <hugo.enscruz@gmail.com>
2019-08-01 18:13:18 -0500

    Se sincronizan los carros una vez que llegan a la meta devolviendolos a su estado original, sin embargo aun falta que ambos sincronicen su movimiento, por el momento solo se ejecuta en los clientes pero no en el servidor

--- Assets/ScriptsGoKart/GoalRace.cs
+++ Assets/ScriptsGoKart/GoalRace.cs
@@ -1,12 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class GoalRace : MonoBehaviour {
+public class GoalRace : NetworkBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    //public GameObject[] respawns;
+    private NetworkStartPosition[] spawnPoints;
+    private GameObject[] respawns;
+
+    // Use this for initialization
+    void Start ()
+    {
+        if(isLocalPlayer)
+        {
+            spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+        }
+            //Debug.Log(spawnPoints);
 	}
 	
 	// Update is called once per frame
@@ -16,6 +26,79 @@ public class GoalRace : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(gameObject.name);
+        if(!isServer)
+        {
+            return;
+        }
+        Debug.Log("Activo el trigger");
+        RpcRespawnPlayers();
+
+    }
+
+    [ClientRpc]
+    public void RpcRespawnPlayers()
+    {
+        //if (isLocalPlayer)
+        //{
+        //Vector3 spawnPoint = Vector3.zero;
+        ////int i = 0;
+        //Debug.Log("Estas aqui");
+        //if (spawnPoints != null && spawnPoints.Length > 0)
+        //{
+        //    //Asignamos aleatoriamente la posición inicial del juagador en el mapa
+        //    spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+        //    Debug.Log(spawnPoint.z);
+        //}
+        //transform.position = spawnPoint;
+        //Debug.Log(gameObject.name);
+        //}
+
+        if (isLocalPlayer)
+        {
+            int i;
+            Vector3 spawnPoint = Vector3.zero;
+            if (respawns == null)
+            {
+                respawns = GameObject.FindGameObjectsWithTag("Respawn");
+            }
+            for (i = 0; i <= 1; i++)
+            {
+                spawnPoint = spawnPoints[i].transform.position;
+                respawns[i].transform.position = spawnPoint;
+            }
+        }
+
+
+
+        //NetworkServer.Spawn(gameObject);
+
+        /*
+        if (respawns == null)
+        {
+            respawns = GameObject.FindGameObjectsWithTag("Respawn");
+        }
+
+        foreach(GameObject respawn in respawns)
+        {
+            if(spawnPoints != null && spawnPoints.Length > 0)
+            {
+                if(i <= spawnPoints.Length)
+                {
+                    for (int j = i; j <= i && j <= spawnPoints.Length; j++)
+                    {
+                        spawnPoint = spawnPoints[j].transform.position;
+                        NetworkServer.Spawn(respawn);
+                    }
+                }
+                else
+                {
+                    spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+                    NetworkServer.Spawn(respawn);
+                }
+                
+            }
+        }
+
+        respawns = null;*/
     }
 }
